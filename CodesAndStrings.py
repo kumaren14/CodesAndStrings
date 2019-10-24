3ff3fe67f1f6146c52ee0fa39f348de6088f57b8
kumaren14 <kumaren.a@gmail.com>
2019-10-24 15:27:50 -0500

    third commit of 10/24

--- CodesAndStrings.py
+++ CodesAndStrings.py
@@ -4,11 +4,31 @@
 #   2 or more strings and put them together
 
 firstName = "Ian"
+
 lastName = "Malcom"
 
-print(firstName + " " + lastName)
-
 name = firstName + " " + lastName
+
 lastFirst = lastName + ", " + firstName
+
+print(firstName + " " + lastName)
 print(name)
 print(lastFirst)
+
+
+print("""                                    
+                                        """)
+
+
+# repetition
+#     repetition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, " + "row, "*2 + "your boat,")
+    print("Gently down the stream.")
+    print("Merrily, " +"merrily, "*3)
+    print("Life is but a dream.")
+
+rowYourBoat()
